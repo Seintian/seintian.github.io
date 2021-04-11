dab7b4ad9d973313bd1a153e37184a8166abde0d
Seintian <67272261+Seintian@users.noreply.github.com>
2021-04-11 20:02:57 +0200

    added topnav with js effects

--- gotico_arte/scripts/script.js
+++ gotico_arte/scripts/script.js
@@ -1,3 +1,45 @@
 window.onload = (event) => {
     AOS.init();
 }
+
+window.onscroll = function() {
+    scroll_navbar();
+    update_navbar();
+};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+var gotico = document.getElementById("gotico").offsetTop;
+var gotico_spagnolo = document.getElementById("gotico_spagnolo").offsetTop;
+var leon = document.getElementById("cattedrale_Leòn").offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. 
+// Remove "sticky" when you leave the scroll position
+function scroll_navbar() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky");
+    } else {
+        navbar.classList.remove("sticky");
+    }
+}
+
+function update_navbar() {
+    if (window.pageYOffset >= leon) {
+        document.getElementById("nav_3").classList.add("bordered");
+        document.getElementById("nav_2").classList.remove("bordered");
+        document.getElementById("nav_1").classList.remove("bordered");
+
+    } else if (window.pageYOffset >= gotico_spagnolo) {
+        document.getElementById("nav_2").classList.add("bordered");
+        document.getElementById("nav_1").classList.remove("bordered");
+        document.getElementById("nav_3").classList.remove("bordered");
+
+    } else {
+        document.getElementById("nav_1").classList.add("bordered");
+        document.getElementById("nav_2").classList.remove("bordered");
+    }
+}
