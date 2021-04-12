97872d7fa5e2640069ca05d2362323a1da4fd46e
Seintian <67272261+Seintian@users.noreply.github.com>
2021-04-12 18:56:45 +0200

    improvements

--- gotico_arte/scripts/script.js
+++ gotico_arte/scripts/script.js
@@ -9,6 +9,11 @@ window.onscroll = function() {
 
 // Get the navbar
 var navbar = document.getElementById("navbar");
+var gotico_nav = document.getElementById("nav_1");
+var gotico_spagnolo_nav = document.getElementById("nav_2");
+var leon_nav = document.getElementById("nav_3");
+var siviglia_nav = document.getElementById("nav_4");
+var tarragona_nav = document.getElementById("nav_5");
 
 // Get the offset position of the navbar
 var sticky = navbar.offsetTop;
@@ -16,6 +21,8 @@ var sticky = navbar.offsetTop;
 var gotico = document.getElementById("gotico").offsetTop;
 var gotico_spagnolo = document.getElementById("gotico_spagnolo").offsetTop;
 var leon = document.getElementById("cattedrale_Leòn").offsetTop;
+var siviglia = document.getElementById("cattedrale_Siviglia").offsetTop;
+var tarragona = document.getElementById("cattedrale_Tarragona").offsetTop;
 
 // Add the sticky class to the navbar when you reach its scroll position. 
 // Remove "sticky" when you leave the scroll position
@@ -28,18 +35,62 @@ function scroll_navbar() {
 }
 
 function update_navbar() {
-    if (window.pageYOffset >= leon) {
-        document.getElementById("nav_3").classList.add("bordered");
-        document.getElementById("nav_2").classList.remove("bordered");
-        document.getElementById("nav_1").classList.remove("bordered");
+    if (window.pageYOffset >= tarragona) {
+        tarragona_nav.classList.add("bordered");
+        siviglia_nav.classList.remove("bordered");
+        
+        tarragona_nav.onmouseover = function () {
+            tarragona_nav.classList.add("hovered");
+        }
+        tarragona_nav.onmouseover = function () {
+            tarragona_nav.classList.remove("hovered");
+        }
+
+    } else if (window.pageYOffset >= siviglia) {
+        tarragona_nav.classList.remove("bordered");
+        siviglia_nav.classList.add("bordered");
+        leon_nav.classList.remove("bordered");
+
+        siviglia_nav.onmouseover = function () {
+            siviglia_nav.classList.add("hovered");
+        }
+        siviglia_nav.onmouseout = function () {
+            siviglia_nav.classList.remove("hovered");
+        }
+
+    } else if (window.pageYOffset >= leon) {
+        siviglia_nav.classList.remove("bordered");
+        leon_nav.classList.add("bordered");
+        gotico_spagnolo_nav.classList.remove("bordered");
+
+        leon_nav.onmouseover = function () {
+            leon_nav.classList.add("hovered");
+        }
+        leon_nav.onmouseout = function () {
+            leon_nav.classList.remove("hovered");
+        }
 
     } else if (window.pageYOffset >= gotico_spagnolo) {
-        document.getElementById("nav_2").classList.add("bordered");
-        document.getElementById("nav_1").classList.remove("bordered");
-        document.getElementById("nav_3").classList.remove("bordered");
+        leon_nav.classList.remove("bordered");
+        gotico_spagnolo_nav.classList.add("bordered");
+        gotico_nav.classList.remove("bordered");
+
+        gotico_spagnolo_nav.onmouseover = function () {
+            gotico_spagnolo_nav.classList.add("hovered");
+        }
+        gotico_spagnolo_nav.onmouseout = function () {
+            gotico_spagnolo_nav.classList.remove("hovered");
+        }
 
     } else {
-        document.getElementById("nav_1").classList.add("bordered");
-        document.getElementById("nav_2").classList.remove("bordered");
+        gotico_spagnolo_nav.classList.remove("bordered");
+        gotico_nav.classList.add("bordered");
+
+        gotico_nav.onmouseover = function () {
+            gotico_nav.classList.add("hovered");
+        }
+        gotico_nav.onmouseout = function () {
+            gotico_nav.classList.remove("hovered");
+        }
     }
 }
