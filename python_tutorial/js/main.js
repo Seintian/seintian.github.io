5e8f80cf5001bb399f5d9cfdb0504d373cc4458b
Seintian <67272261+Seintian@users.noreply.github.com>
2021-05-08 21:54:23 +0200

    variables done

--- python_tutorial/js/main.js
+++ python_tutorial/js/main.js
@@ -1,13 +1,15 @@
 var toggle_button = document.getElementById("toggle-dark-mode");
 var menu = document.getElementById("menu");
+var articleContent = document.getElementById("content");
+var topbar = document.getElementById("topbar");
 
 function toggleDarkMode() {
     if (document.body.classList.contains("dark-mode")) {
         document.body.classList.remove("dark-mode");
         document.body.classList.add("light-mode");
 
-        document.getElementById("topbar").classList.remove("dark-mode");
-        document.getElementById("topbar").classList.add("light-mode");
+        topbar.classList.remove("dark-mode");
+        topbar.classList.add("light-mode");
 
         toggle_button.innerHTML = '<img src="img/dark_mode-icon.png" alt="Toggle Dark Mode" width="32px" id="toggle-img">';
     
@@ -15,8 +17,8 @@ function toggleDarkMode() {
         document.body.classList.remove("light-mode");
         document.body.classList.add("dark-mode");
 
-        document.getElementById("topbar").classList.remove("light-mode");
-        document.getElementById("topbar").classList.add("dark-mode");
+        topbar.classList.remove("light-mode");
+        topbar.classList.add("dark-mode");
 
         toggle_button.innerHTML = '<img src="img/light_mode-icon.png" alt="Toggle Light Mode" width="32px">';
     
@@ -27,9 +29,9 @@ function toggleDarkMode() {
 function toggleMenu() {
     if (menu.style.display == "none") {
         menu.style.display = "block";
-        document.getElementById("content").style.margin = "0";
+        articleContent.style.margin = "0";
     } else {
         menu.style.display = "none";
-        document.getElementById("content").style.margin = "0 20%";
+        articleContent.style.margin = "0 20%";
     }
 }
